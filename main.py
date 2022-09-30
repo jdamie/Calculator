6c3179ad76656c914882c95ce587a583b0a9f3ec
jdamie <63238484+jdamie@users.noreply.github.com>
2022-09-30 21:46:20 +0900

    0.0.2
    minor error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 view = [100,200,400,800]
-sview = pd.series(view)
+sview = pd.Series(view)
 st.write("# Mark-up level 1")
 st.write("## Mark-up level 2")
 st.bar_chart(view)
